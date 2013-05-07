8cfa7a2f8b5a14120e23a72a09fb4328ee95f43c
Jakub Nabrdalik <jakubn@gmail.com>
2013-05-07 10:16:07 +0200

    removed touk maven repo, since it is no longer needed (the plugin is in official Grails repo)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
     checksums true // Whether to verify checksums on resolve
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+        inherits false // Whether to inherit repository definitions from plugins
 
         grailsPlugins()
         grailsHome()
@@ -26,14 +26,9 @@ grails.project.dependency.resolution = {
         mavenLocal()
         mavenCentral()
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-
-        mavenRepo("http://maven.touk.pl/nexus/content/repositories/releases")
+        //mavenRepo("http://maven.touk.pl/nexus/content/repositories/releases")
     }
+
     dependencies {
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
@@ -46,7 +41,6 @@ grails.project.dependency.resolution = {
             excludes 'groovy-all', "spock-grails-support"
         }
 
-        runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.2"
         runtime ":resources:1.1.6"
 
