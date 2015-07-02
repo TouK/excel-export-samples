fda25046308b9b2c72d13e8ced342a43d4d3fc86
mansiarora <mansi@intelligrape.com>
2015-07-02 17:50:18 +0530

    Updated package settings;

--- grails-app/init/pl/tour/excel/export/samples/Application.groovy
+++ grails-app/init/pl/tour/excel/export/samples/Application.groovy
@@ -1,8 +1,10 @@
-package excel.export.samples
+package pl.tour.excel.export.samples
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
+@ComponentScan(basePackages = ['pl.touk.excel.export.samples.samples', 'pl.touk.excel.export.samples'])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
